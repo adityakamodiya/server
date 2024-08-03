fb95ee5124d889cd042f5d3e63ba1017dc946425
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-08-04 00:10:25 +0530

    commit

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(express.urlencoded({ extended: false }))
 
  configDotenv();
 cloudinary.config({
-  cloud_name: process.env.cloud_name,
-  api_key: process.env.api_key,
-  api_secret: process.env.api_key,
+  cloud_name: 'da2oqj7qe',
+  api_key: '687377994928293',
+  api_secret: 'GcXxtuXnuQ-LJGycDcmf_DGqw_E'
 });
 
 
