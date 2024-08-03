34a44822f01f8e485b3824216f3f8a311ef6cd5a
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-08-04 01:02:15 +0530

    ...

--- connection.js
+++ connection.js
@@ -1,5 +1,7 @@
 import{MongoClient} from "mongodb"
-const connection = MongoClient.connect("mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/MyApi?retryWrites=true&w=majority&appName=Cluster0");
+import { configDotenv } from "dotenv";
+configDotenv();
+const connection = MongoClient.connect(process.env.Cstring);
 export const dbName = "MyApi"
 export default connection
 
