11e6a054a17f9e2fdc65909736b71a1ce8e58b84
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-08-03 23:40:21 +0530

    commit

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
-import{MongoClient} from "mongodb"
-import { configDotenv } from "dotenv";
+import { MongoClient } from "mongodb";
+import { config } from "dotenv";
 
-configDotenv()
-const connection = MongoClient.connect(`${process.env.mystring}`);
-export const dbName = `${process.env.dbName}`;
+config();
+const connection = MongoClient.connect(process.env.MONGO_URI);
+export const dbName = process.env.DB_NAME;
 export default connection
 
 // mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/fileuploader?retryWrites=true&w=majority&appName=Cluster0
